ba186c2abd9dd6cadf4219ee92733b89948a2583
Takafumi Umemoto <umecch@gmail.com>
2018-10-08 07:35:25 +0900

    別のファイルに分けた

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,8 @@
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add,),
-        onPressed: () {
-          print('Hi there!');
-        },
-      ),
-      appBar: AppBar(
-        title: Text('Lets see some images!')
-      ),
-    )
-  );
+  var app = new App();
 
   runApp(app);
 }
