904c78d240c9174f1801741153af6db49836de41
Takafumi Umemoto <umecch@gmail.com>
2018-10-07 16:25:29 +0900

    Scaffoldクラスを使ってAppBarを表示

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 void main() {
   var app = MaterialApp(
-    home: Text('Hi there!'),
+    home: Scaffold(
+      appBar: AppBar(),
+    )
   );
 
   runApp(app);
