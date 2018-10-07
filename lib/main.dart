3d4934dddd6f0d771fa118e70546669e6f285bc6
Takafumi Umemoto <umecch@gmail.com>
2018-10-07 16:28:34 +0900

    AppBarにタイトルを表示

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     home: Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Lets see some images!')
+      ),
     )
   );
 
